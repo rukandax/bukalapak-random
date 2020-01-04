6fa2ff52f42ebcfafcefc36e76bdc1f08df22e96
Rukanda Faridsi <rukanda.faridsi@bukalapak.com>
2020-01-04 11:44:11 +0700

    Project Source

---
+++ script.js
@@ -0,0 +1,129 @@
+const queryParams = extractParams() || {};
+
+if (queryParams.category_id) {
+  $("#product-filter-category").show();
+}
+
+$("#toggle-filter-category").on("click", function() {
+  $("#product-filter-category").slideToggle();
+});
+
+function getCategories(token) {
+  $.ajax({
+    crossDomain: true,
+    url: "https://api.bukalapak.com/categories?access_token=" + token,
+    success: function(data) {
+      data.data.forEach(category => {
+        let selected = "";
+        if (queryParams.category_id == category.id) {
+          selected = 'checked="checked"';
+        }
+
+        $("#product-filter-category").append(`
+          <div class="product-filter-category-item">
+            <input type="radio" name="category" id="radio-${category.id}" value="${category.id}" ${selected} onclick="window.location = window.location.pathname + '?category_id=${category.id}'">
+            <label for="radio-${category.id}">${category.name}</label>
+          </div>
+        `);
+      });
+    }
+  });
+}
+
+var firstTry = true;
+
+function getProducts(token, offset = 0) {
+  $.ajax({
+    crossDomain: true,
+    url:
+      "https://api.bukalapak.com/products?assurance=false&category_id=" +
+      (queryParams.category_id || "") +
+      "&original=true&condition=new&limit=50&offset=" +
+      offset +
+      "&rating=4%3A5&sort=date&top_seller=true&access_token=" +
+      token,
+    success: function(data) {
+      if (firstTry) {
+        firstTry = false;
+        const total = data.meta.total;
+        let selectedOffset = offset;
+
+        if (total > 50) {
+          selectedOffset = parseInt(Math.random() * (total - 50));
+        }
+
+        getProducts(token, selectedOffset);
+      } else {
+        const products = data.data;
+        let randomizedIndex = parseInt(Math.random() * products.length);
+        let product = products[randomizedIndex];
+
+        if (getCookie("last-item").length <= 0) {
+          setCookie("last-item", "[]");
+        }
+
+        const lastItem = JSON.parse(getCookie("last-item"));
+
+        while (lastItem.includes(product.sku_id)) {
+          randomizedIndex = parseInt(Math.random() * products.length);
+          product = products[randomizedIndex];
+        }
+
+        if (lastItem.length >= 25) {
+          lastItem.shift();
+        }
+
+        lastItem.push(product.sku_id);
+        setCookie("last-item", JSON.stringify(lastItem));
+
+        const productUrl = mobileCheck()
+          ? product.url
+          : "https://bukalapak.go2cloud.org/aff_c?offer_id=15&aff_id=7049&url=" +
+            encodeURIComponent(
+              product.url +
+                "?ho_offer_id={offer_id}&ho_trx_id={transaction_id}&affiliate_id={affiliate_id}&utm_source=hasoffers&utm_medium=affiliate&utm_campaign={offer_id}&ref={referer}"
+            );
+
+        $("#product-image img").attr("src", product.images.large_urls[0]);
+        $("#product-image a").attr("href", productUrl);
+        $("#product-title a").attr("href", productUrl);
+        $("#product-title a").text(product.name);
+        $("#product-price").text(
+          "Rp " + product.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")
+        );
+        $("#product-footer a").attr(
+          "href",
+          mobileCheck()
+            ? "https://bl.id/bayar/" + product.sku_id.toString(36)
+            : productUrl
+        );
+        $("#product-image").attr("style", "");
+
+        console.log(`Collected Products Length : ${products.length}`);
+        console.log(`Total Products Length : ${data.meta.total}`);
+        console.log(`Offset Products : ${offset}`);
+        console.log(`Random Index : ${randomizedIndex}`);
+      }
+    }
+  });
+}
+
+function getToken() {
+  $.ajax({
+    crossDomain: true,
+    url: "https://cors-anywhere.herokuapp.com/https://m.bukalapak.com",
+    success: function(data) {
+      const token = data.split('access_token:"')[1].split('"')[0];
+
+      if (token) {
+        getCategories(token);
+        getProducts(token);
+      } else if (tryAttemp <= 3) {
+        tryAttemp += 1;
+        getToken();
+      }
+    }
+  });
+}
+
+getToken();
